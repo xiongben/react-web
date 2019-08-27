51b1e3bec5c019f0fc2b8706f9b97b1ebd726a3b
xiongben <xiongben@mozat.com>
2019-08-27 17:05:27 +0800

    add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'video-react/dist/video-react.css';
+import { Player } from 'video-react';
+import { Button } from 'antd-mobile';
+;
 
 function App() {
   return (
@@ -10,14 +14,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="box">
+        <Player>
+          <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+        </Player>
+        <Button style={{width:'100px'}}>Start</Button>
+        </div>
       </header>
     </div>
   );
